0bf404870002ba185129d4e7435e9e4f16a54fc2
thomasuriel1 <tlauravargas@gmail.com>
2024-05-02 11:25:11 -0300

    ejercicio 4

--- simulacro peliculas.sql
+++ simulacro peliculas.sql
@@ -1,3 +1,10 @@
 1- SELECT title from film
-   ORDER BY title ASC
-2- SELECT 
+   ORDER BY title ASC;
+2- SELECT DISTINCT rating from film
+   ORDER BY rating ASC;
+3- SELECT DISTINCT rating,rental_rate,title FROM film
+   WHERE rating LIKE "%PG-13%" OR "%PG%"
+   ORDER BY rental_rate DESC, title ASC;
+4- SELECT city, co.country FROM city c JOIN country co ON c.country_id
+   GROUP BY country;
+5- 
