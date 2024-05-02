928bebbc680228d062a650f4efedde04a456859c
thomasuriel1 <tlauravargas@gmail.com>
2024-05-02 12:11:56 -0300

    ejercicio 8

--- simulacro peliculas.sql
+++ simulacro peliculas.sql
@@ -10,3 +10,10 @@
 5- SELECT city as ciudad,COUNT(*) AS ciudades, co.country as pais FROM city c JOIN country co ON c.country_id = co.country_id
    GROUP BY country
    ORDER BY ciudades DESC;
+6- SELECT city as ciudad,COUNT(*) AS ciudades, co.country as pais FROM city c JOIN country co ON c.country_id = co.country_id
+   GROUP BY country HAVING ciudades > 2
+   ORDER BY ciudades DESC;
+7- SELECT MIN(rental_date) AS fecha_mas_vieja , MAX(rental_date) AS fecha_mas_nueva FROM rental;
+8- SELECT actor_id, fi.title FROM film_actor f JOIN film fi ON f.film_id = fi.film_id
+   GROUP BY fi.film_id
+   ORDER BY actor_id ASC LIMIT 10; 
