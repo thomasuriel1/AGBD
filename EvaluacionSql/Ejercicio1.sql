cece00ed7f05a3505e74d4ac1ea4b65f460fc108
thomasuriel1 <tlauravargas@gmail.com>
2024-05-16 10:32:18 -0300

    ejercicio 1

---
+++ EvaluacionSql/Ejercicio1.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT type from crime_scene_report
+ORDER BY type ASC;
---
+++ EvaluacionSql/consignas_evaluacion.sql
@@ -0,0 +1,38 @@
+-- 1) Mostrar todos los tipos de crimenes
+-- reportados en la ciudad 'SQL City'.
+-- Los tipos de crimen deben estar en orden 
+-- alfabético y no debe haber repetidos
+
+-- 2) Listar el nombre de la persona y los 
+-- datos del auto (marca, modelo y patente) de 
+-- todos las mujeres de menos de 40 años, 
+-- elegir el orden en que se muestran 
+
+-- 3) Buscar la lista de todas las personas
+-- que NO están asociadas al gimnasio Get-Fit-Now
+
+-- 4) Encontrar el nombre y la dirección completa
+-- de la persona que vive en la tercera casa
+-- de la calle 'Franklin Ave' (ordenadas por la altura
+-- de la calle)
+
+-- 5) Encontrar la edad mínima, máxima y promedio
+-- de los varones que tienen licencia de conducir
+
+-- 6) Contar la cantidad de autos de cada
+-- marca, ordenadas desde la que tiene más 
+-- autos a la que tiene menos.  Descartar
+-- las marcas que tienen menos de 100 autos
+
+-- 7) Mostrar los nombres, numeros de seguridad social
+-- (ssn), id de membresía y tipo de membresía de
+-- las personas que fueron al gimnasio aunque sea una
+-- vez durante el 2017.  Los resultados deben estar
+-- ordenados de manera que los de un mismo tipo de
+-- membresía (gold, silver, etc) aparezcan todos 
+-- seguidos y dentro de cada una ordenados por ssn.   
+
+-- 8) Hacer un ranking de los 5 eventos más populares
+-- a los que asistieron las personas que ganan 
+-- más de 35000 dolares al año.  
+
