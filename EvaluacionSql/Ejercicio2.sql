d7487264fafb2b11a6f35f64bd8dcf4597376359
thomasUriel <tlauravargas@gmail.com>
2024-05-16 11:12:53 -0300

    ejercicio 2

--- EvaluacionSql/Ejercicio2.sql
+++ EvaluacionSql/Ejercicio2.sql
@@ -1,1 +1,2 @@
-SELECT name, p.id, car_make, car_model, plate_number FROM person p JOIN drivers_license d ON p.license_id = d.id
+SELECT name, gender, p.id, car_make, car_model, plate_number, age FROM person p JOIN drivers_license d ON p.license_id = d.id
+WHERE gender = "female" and age < "40"
